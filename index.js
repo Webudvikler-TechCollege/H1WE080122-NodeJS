c41c22679f73c1eae3430ad8107f0e8e4aeaa7f7
Heinz K <heka@techcollege.dk>
2022-11-30 08:43:10 +0100

    Index comments

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ import express from 'express'
 import dotenv from 'dotenv';
 import SongRouter from './Routes/song.router.js'
 
+// Kalder dotenv modul
 dotenv.config();
+// Deklarerer port
 const port = process.env.PORT || 3000
-
+// Deklarerer app objekt
 const app = express()
 
 // App settings to provide access to request body data
 app.use(express.urlencoded({ extended: true }))
 
+// Route til forside
 app.get('/', (req, res) => {
 	res.send('Hello world')
 })
 
+// Route bundles
 app.use(SongRouter)
 
+// Run server
 app.listen(port, () => {
 	console.log(`Webserver running on http://localhost:${port}`);
 })
